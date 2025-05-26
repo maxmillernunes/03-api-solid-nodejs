aac4cb4f4c4da42c7ffc0e1f6e2d8c1921343c94
Maxmiller Nunes <maxmillernunes11@gmail.com>
2025-05-25 23:59:32 -0300

    feat: :sparkles: create register controller, password hash and validation email

---
+++ src/http/controllers/register.ts
@@ -0,0 +1,38 @@
+import { FastifyRequest, FastifyReply } from 'fastify'
+import { hash } from 'bcryptjs'
+import { prisma } from '@/lib/prisma'
+import { z } from 'zod'
+
+export async function register(request: FastifyRequest, reply: FastifyReply) {
+  const registerBodySchema = z.object({
+    name: z.string(),
+    email: z.string().email(),
+    password: z.string().min(6),
+  })
+
+  const { email, name, password } = registerBodySchema.parse(request.body)
+
+  const password_hash = await hash(password, 6)
+
+  const userWithSameEmail = await prisma.user.findUnique({
+    where: {
+      email,
+    },
+  })
+
+  if (userWithSameEmail) {
+    return reply.status(409).send({
+      message: 'User already exists with this email',
+    })
+  }
+
+  await prisma.user.create({
+    data: {
+      email,
+      name,
+      password_hash,
+    },
+  })
+
+  return reply.status(201).send()
+}
