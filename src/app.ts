8e76ced68a56538e662c34c9039a17e16e5288a2
Maxmiller Nunes <maxmillernunes11@gmail.com>
2025-05-26 01:09:46 -0300

    feat: :sparkles: add global error handling

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,27 @@
 import fastify from 'fastify'
+import { ZodError } from 'zod'
 import { appRoutes } from './http/routes'
+import { env } from './env'
 
 export const app = fastify()
 
 app.register(appRoutes)
+
+app.setErrorHandler((error, _request, reply) => {
+  if (error instanceof ZodError) {
+    return reply.status(400).send({
+      message: 'Validation error.',
+      issues: error.format(),
+    })
+  }
+
+  if (env.NODE_ENV !== 'production') {
+    console.error(error)
+  } else {
+    // TODO: Here we should log the error to an external tool, like Sentry/Datadog.
+  }
+
+  return reply.status(500).send({
+    message: 'Internal server error.',
+  })
+})
