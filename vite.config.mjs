9ac4a71d644e4805570f75c66240432636b465a0
Maxmiller Nunes <maxmillernunes11@gmail.com>
2025-06-13 00:57:24 -0300

    test: :white_check_mark: add tests environment using the vitest and postgres to prisma

---
+++ vite.config.mjs
@@ -0,0 +1,27 @@
+import { defineConfig } from 'vitest/config'
+import tsConfigPaths from 'vite-tsconfig-paths'
+
+export default defineConfig({
+  plugins: [tsConfigPaths()],
+  test: {
+    dir: 'src',
+    projects: [
+      {
+        extends: true,
+        test: {
+          name: 'unit',
+          dir: 'src/use-cases',
+        },
+      },
+      {
+        extends: true,
+        test: {
+          name: 'e2e',
+          dir: 'src/http/controllers',
+          environment:
+            './prisma/vitest-environment-prisma/prisma-test-environment.ts',
+        },
+      },
+    ],
+  },
+})
