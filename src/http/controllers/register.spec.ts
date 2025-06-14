6e9e2e0603e2abf1a58f3e3ed58f25e90af8b49e
Maxmiller Nunes <maxmillernunes11@gmail.com>
2025-06-14 17:50:03 -0300

    test(test): :white_check_mark: add e2e tests

--- src/http/controllers/register.spec.ts
+++ src/http/controllers/register.spec.ts
@@ -1,5 +1,23 @@
-import { expect, test } from 'vitest'
+import request from 'supertest'
+import { afterEach, beforeAll, describe, expect, it } from 'vitest'
+import { app } from '@/app'
 
-test('Testing', () => {
-  expect(1 + 1).toEqual(2)
+describe('Register (e2e)', () => {
+  beforeAll(async () => {
+    await app.ready()
+  })
+
+  afterEach(async () => {
+    await app.close()
+  })
+
+  it('Should be able to register', async () => {
+    const response = await request(app.server).post('/users').send({
+      name: 'john doe',
+      email: 'johndoe@gmail.com',
+      password: 'asdqwe',
+    })
+
+    expect(response.statusCode).toEqual(201)
+  })
 })
